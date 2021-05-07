e60844a94d570a947463e1cb6f43fdc80e983e43
Eugene <Eugeniolio@yandex.com>
2021-05-08 00:39:39 +0300

    Initial Commit

---
+++ EugeneApp/EugeneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EugeneAppApp.swift
+//  EugeneApp
+//
+//  Created by Евгений Фирман on 08.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct EugeneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
